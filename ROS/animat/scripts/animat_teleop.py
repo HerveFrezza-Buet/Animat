318fcfeb44b24c5feb58ff08bb76a0d89a1324f2
Hervé Frezza-Buet <herve.frezza-buet@entralesupelec.fr>
2017-06-05 09:52:11 +0200

    scene fixed

--- ROS/animat/scripts/animat_teleop.py
+++ ROS/animat/scripts/animat_teleop.py
@@ -16,7 +16,7 @@ def main(stdscr):
     stdscr.addstr("Command\n")
     stdscr.addstr(" - UP/DOWN    : go/stop\n")
     stdscr.addstr(" - LEFT/RIGHT : turn left/right\n")
-    stdscr.addstr(" - b/r/d      : focus on blue, red or don't care.\n")
+    stdscr.addstr(" - b/r/d/+    : focus on blue, red or don't care, + resets the focus.\n")
     stdscr.addstr(" - <space>    : ingest.\n")
     # We set the "wait for a key press" period to 100 ms. 
     while (not rospy.is_shutdown()) : 
@@ -26,10 +26,11 @@ def main(stdscr):
         elif keycode == curses.KEY_DOWN  : bpub.publish("stop")
         elif keycode == curses.KEY_LEFT  : bpub.publish("left")
         elif keycode == curses.KEY_RIGHT : bpub.publish("right")
-        elif keycode == 32               : bpub.publish("ingest")   # space
-        elif keycode == 98               : vpub.publish("blue")     # b
+        elif keycode ==  32              : bpub.publish("ingest")   # space
+        elif keycode ==  98              : vpub.publish("blue")     # b
         elif keycode == 114              : vpub.publish("red")      # r
         elif keycode == 100              : vpub.publish("dontcare") # d
+        elif keycode ==  43              : vpub.publish("reset")    # +
         else                             : pass
         rate.sleep()
 
